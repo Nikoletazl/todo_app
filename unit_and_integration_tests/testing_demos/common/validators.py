f70ea6ff260de62b9d7ef982f8caa1f190ddc0ad
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-04-04 20:27:57 +0300

    Delete unit_and_integration_tests/testing_demos directory

--- unit_and_integration_tests/testing_demos/common/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_only_letters(value):
-    invalid_chars = [ch for ch in value if not ch.isalpha]
-    if invalid_chars:
-        raise ValidationError(f'Value must contain only letters, but contains: {invalid_chars}')
